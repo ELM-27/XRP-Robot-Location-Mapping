8da711e171b8696a41f3241a67c36825dc25803f
Elliott Lowman <158524329+ELM-27@users.noreply.github.com>
2026-04-21 16:26:16 -0400

    standardized functions

---
+++ Rand Movement/functions.py
@@ -0,0 +1,26 @@
+from XRPLib.defaults import *
+from XRPLib.board import Board
+from XRPLib.reflectance import Reflectance
+from XRPLib.differential_drive import DifferentialDrive
+import time
+import random
+import math
+
+def stdFileWrite(recordFile, movements) :
+    f.write(movements + "\n")
+    
+def fmt1(v):
+    return str(round(v, 1))
+    
+def fmt3(v):
+    return str(round(v, 3))
+
+def rDriveStraight(startEffort, recordFile) :
+    drivetrain.set_effort(startEffort, startEffort)
+    currentYaw = imu.get_yaw()
+    stdFileWrite(recordFile, "Drive: " + fmt1(currentYaw) + " " + fmt3(startEffort) + " " + fmt3(startEffort) + " " + str(time.ticks_ms()))
+    
+def rDrive(leftEffort, rightEffort, recordFile) :
+    drivetrain.set_effort(leftEffort, rightEffort)
+    currentYaw = imu.get_yaw()
+    stdFileWrite(recordFile, "Drive: " + fmt1(currentYaw) + " " + fmt3(leftEffort) + " " + fmt3(rightEffort) + " " + str(time.ticks_ms()))
