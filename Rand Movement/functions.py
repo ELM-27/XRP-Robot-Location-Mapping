f82adf29a74fc3b5f5f64dc91766fe554a2da5f1
Elliott Lowman <158524329+ELM-27@users.noreply.github.com>
2026-04-28 11:19:12 -0400

    Add functions for turning, reversing, and stopping

--- Rand Movement/functions.py
+++ Rand Movement/functions.py
@@ -24,3 +24,16 @@ def rDrive(leftEffort, rightEffort, recordFile) :
     drivetrain.set_effort(leftEffort, rightEffort)
     currentYaw = imu.get_yaw()
     stdFileWrite(recordFile, "Drive: " + fmt1(currentYaw) + " " + fmt3(leftEffort) + " " + fmt3(rightEffort) + " " + str(time.ticks_ms()))
+
+def rTurn(degrees, startEffort, recordFile) :
+    stdFileWrite(recordFile, "Pause: " + str(time.tick_ms()))
+    drivetrain.turn(degrees, startEffort)
+
+def rReverse(startEffort, recordFile) :
+    drivetrain.set_effort(-startEffort, -startEffort)
+    currentYaw = imu.get_yaw()
+    stdFileWrite(recordFile, "Reverse: " + fmt1(currentYaw) + " " + fmt3(-startEffort) + " " + fmt3(-startEffort) + " " + str(time.ticks_ms()))
+    
+def rStop(recordFile) :
+    stdFileWrite(recordFile, "Pause: " + str(time.tick_ms()))
+    drivetrain.stop()
